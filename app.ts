03d8a35025c65a2261e7e4263bce59bd57d254a9
Joshua Verdehem <joshverd2@gmail.com>
2023-08-21 10:28:09 -0500

    Added CORS setup to express

--- app.ts
+++ app.ts
@@ -6,6 +6,12 @@ const PORT = 3001;
 // App initialization
 const app = express();
 
+// CORS
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+});
+
 // Routes
 app.use('/api/example', require('./routes/example'));
 
